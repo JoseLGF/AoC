e99eccb48661293a4182f96e65c92b99bd18883d
Jose Luis <luisgermanfelix@gmail.com>
2022-01-23 20:11:15 +0100

    Add solution to day 3 part 2

--- 2021/build.sh
+++ 2021/build.sh
@@ -1,7 +1,7 @@
-g++ -c src/day01.cpp
-g++ -c src/day02.cpp
-g++ -c src/day03.cpp
-g++ -c src/main.cpp
+g++ -c -g src/day01.cpp
+g++ -c -g src/day02.cpp
+g++ -c -g src/day03.cpp
+g++ -c -g src/main.cpp
 g++ *.o -o aoc2021
 
 # Clean
--- 2021/src/day03.cpp
+++ 2021/src/day03.cpp
@@ -1,84 +1,128 @@
 #include <fstream>
 #include <vector>
 #include <iostream>
-#include <forward_list>
+#include <string>
+
+char most_common_bit_at_position(const std::vector<std::string>& values, int position)
+{
+    int bitcount = 0;
+
+    for (auto code : values) {
+        char bit_c = code[position];
+        if (bit_c == '1') {
+            bitcount++;
+        }
+        else {
+            bitcount--;
+        }
+    }
+
+    if (bitcount >= 0) return '1';
+    return '0';
+}
+
+char least_common_bit_at_position(const std::vector<std::string>& values, int position)
+{
+    int bitcount = 0;
+
+    for (auto code : values) {
+        char bit_c = code[position];
+        if (bit_c == '1') {
+            bitcount++;
+        }
+        else {
+            bitcount--;
+        }
+    }
+
+    if (bitcount >= 0) return '0';
+    return '1';
+}
 
 int day03_1()
 {
     std::ifstream infile("input/day03.txt");
-    std::string diagnostic;
-    std::vector<int> bitCounts(12, 0);
-
-    while (infile >> diagnostic) {
-        bitCounts[0] +=  ((diagnostic[0] == '1') ? 1 : -1);
-        bitCounts[1] +=  ((diagnostic[1] == '1') ? 1 : -1);
-        bitCounts[2] +=  ((diagnostic[2] == '1') ? 1 : -1);
-        bitCounts[3] +=  ((diagnostic[3] == '1') ? 1 : -1);
-        bitCounts[4] +=  ((diagnostic[4] == '1') ? 1 : -1);
-        bitCounts[5] +=  ((diagnostic[5] == '1') ? 1 : -1);
-        bitCounts[6] +=  ((diagnostic[6] == '1') ? 1 : -1);
-        bitCounts[7] +=  ((diagnostic[7] == '1') ? 1 : -1);
-        bitCounts[8] +=  ((diagnostic[8] == '1') ? 1 : -1);
-        bitCounts[9] +=  ((diagnostic[9] == '1') ? 1 : -1);
-        bitCounts[10] += ((diagnostic[10] == '1') ? 1 : -1);
-        bitCounts[11] += ((diagnostic[11] == '1') ? 1 : -1);
+    std::vector<std::string> codes;
+    std::string str;
+    while (getline(infile, str)) {
+        codes.push_back(str);
     }
 
     unsigned int gamma = 0;
-    if (bitCounts[0] > 0)  gamma |= 0x800;
-    if (bitCounts[1] > 0)  gamma |= 0x400;
-    if (bitCounts[2] > 0)  gamma |= 0x200;
-    if (bitCounts[3] > 0)  gamma |= 0x100;
-    if (bitCounts[4] > 0)  gamma |= 0x080;
-    if (bitCounts[5] > 0)  gamma |= 0x040;
-    if (bitCounts[6] > 0)  gamma |= 0x020;
-    if (bitCounts[7] > 0)  gamma |= 0x010;
-    if (bitCounts[8] > 0)  gamma |= 0x008;
-    if (bitCounts[9] > 0)  gamma |= 0x004;
-    if (bitCounts[10] > 0) gamma |= 0x002;
-    if (bitCounts[11] > 0) gamma |= 0x001;
-
+    for (int i=0; i<12; i++)  {
+        if('1' == most_common_bit_at_position(codes, i)) {
+            gamma |= (1 << (11 - i));
+        }
+    }
     return gamma * ((~gamma) & 0xFFF);
+}
+
+unsigned int bitstring_to_uint(std::string bitstring)
+{
+    unsigned int result = 0;
+    for (int i=bitstring.size()-1; i>=0; i--) {
+        char bit_c = bitstring[i];
+        if (bit_c == '1') {
+            result += (1 << (bitstring.size()-1-i));
+        }
+    }
+    return result;
+}
+
+unsigned int get_oxygen_generator_rating(std::vector<std::string> values, int numBits)
+{
+    int current_bit = 0;
+    while(values.size() > 1) {
+        char bit_to_filter = most_common_bit_at_position(values, current_bit);
+        for (int i=values.size()-1; i>=0; i--) {
+            if (
+               ( (bit_to_filter == '0') && (values[i][current_bit] == '1') ) ||
+               ( (bit_to_filter == '1')  && (values[i][current_bit] == '0') )
+               )
+            {
+                values.erase(values.begin() + i);
+            }
+        }
+        current_bit++;
+    }
+
+    unsigned int og = bitstring_to_uint(values[0]);
+    return og;
+}
+
+unsigned int get_co2_scrubber_rating(std::vector<std::string> values, int numBits)
+{
+    int current_bit = 0;
+    while(values.size() > 1) {
+        char bit_to_filter = least_common_bit_at_position(values, current_bit);
+        for (int i=values.size()-1; i>=0; i--) {
+            if (
+               ( (bit_to_filter == '0') && (values[i][current_bit] == '1') ) ||
+               ( (bit_to_filter == '1')  && (values[i][current_bit] == '0') )
+               )
+            {
+                values.erase(values.begin() + i);
+            }
+        }
+        current_bit++;
+    }
+
+    unsigned int co = bitstring_to_uint(values[0]);
+    return co;
 }
 
 int day03_2()
 {
     std::ifstream infile("input/day03.txt");
-    std::string diagnostic;
-    std::vector<int> bitCounts(12, 0);
-    std::forward_list<std::string> oxygen_numbers;
-    std::forward_list<std::string> co2_numbers;
-
-    while (infile >> diagnostic) {
-        oxygen_numbers.push_front(diagnostic);
-        co2_numbers.push_front(diagnostic);
-        bitCounts[0] +=  ((diagnostic[0] == '1') ? 1 : -1);
-        bitCounts[1] +=  ((diagnostic[1] == '1') ? 1 : -1);
-        bitCounts[2] +=  ((diagnostic[2] == '1') ? 1 : -1);
-        bitCounts[3] +=  ((diagnostic[3] == '1') ? 1 : -1);
-        bitCounts[4] +=  ((diagnostic[4] == '1') ? 1 : -1);
-        bitCounts[5] +=  ((diagnostic[5] == '1') ? 1 : -1);
-        bitCounts[6] +=  ((diagnostic[6] == '1') ? 1 : -1);
-        bitCounts[7] +=  ((diagnostic[7] == '1') ? 1 : -1);
-        bitCounts[8] +=  ((diagnostic[8] == '1') ? 1 : -1);
-        bitCounts[9] +=  ((diagnostic[9] == '1') ? 1 : -1);
-        bitCounts[10] += ((diagnostic[10] == '1') ? 1 : -1);
-        bitCounts[11] += ((diagnostic[11] == '1') ? 1 : -1);
+    std::vector<std::string> codes;
+    std::string str;
+    while (getline(infile, str)) {
+        codes.push_back(str);
     }
 
-    int current_bit = 0;
-    // Find the oxygen generator rating
-    while (std::distance(oxygen_numbers.begin(),
-                         oxygen_numbers.end()) != 1)
-    {
-        bool most_common_bit = bitCounts[0] >= 0 ? true : false;
-
-    }
-
-
-    int oxygen_generator_rating;
-    int CO2_scrubber_rating;
-    int life_support_rating =
+    unsigned int oxygen_generator_rating = get_oxygen_generator_rating(codes, 12);
+    unsigned int CO2_scrubber_rating = get_co2_scrubber_rating(codes, 12);
+    unsigned int life_support_rating =
         oxygen_generator_rating * CO2_scrubber_rating;
-    return -1;
+    return life_support_rating;
 }
