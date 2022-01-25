799f50dbfeb4ba32499a68c579212387e056afa3
Jose Luis <luisgermanfelix@gmail.com>
2022-01-25 22:25:23 +0100

    Add solution to day 6 part 2

--- 2021/src/day06.cpp
+++ 2021/src/day06.cpp
@@ -4,10 +4,58 @@
 #include <sstream>
 #include <string>
 #include <algorithm>
+#include <numeric>
+#include <cstdint>
 
-std::vector<int> simulate_fish(std::vector<int> state)
+class Fish_simulator
 {
-    std::vector<int> out;
+    int num_buckets = 9;
+    std::vector<uint64_t> fish_buckets;
+
+public:
+
+    Fish_simulator(std::vector<unsigned int> initial_state) {
+        fish_buckets = std::vector<uint64_t>(num_buckets,0);
+        // Load buckets
+        for (auto f: initial_state) {
+            fish_buckets[f] ++;
+        }
+    }
+
+    void simulate() {
+        uint64_t new_fish = 0;
+        for (int i=0; i<num_buckets; i++) {
+            if (0 == i) {
+                new_fish = fish_buckets[0];
+            }
+            else {
+                fish_buckets[i-1] = fish_buckets[i];
+            }
+        }
+        // Update new fish
+        fish_buckets[8] = new_fish;
+        fish_buckets[6] += new_fish;
+    }
+
+    void print_buckets() {
+        std::cout << "Buckets: ";
+        for (auto b : fish_buckets) {
+            std::cout << b << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    uint64_t get_num_fish() {
+        return std::accumulate(fish_buckets.begin(),
+                                                fish_buckets.end(),
+                                                (uint64_t)0);
+    }
+
+};
+
+std::vector<unsigned int> simulate_fish(std::vector<unsigned int> state)
+{
+    std::vector<unsigned int> out;
     for(auto f: state) {
         if (f >= 1) {
             out.push_back(f-1);
@@ -27,8 +75,8 @@ void day06()
 
     int num_days = 80;
 
-    std::vector<int> initial_state;
-    int f;
+    std::vector<unsigned int> initial_state;
+    unsigned int f;
     char c;
     while (infile >> f)
     {
@@ -43,7 +91,7 @@ void day06()
     std::cout << std::endl;
     */
 
-    std::vector<int> fish = initial_state;
+    std::vector<unsigned int> fish = initial_state;
 
     for (int i=0; i<num_days; i++) {
         fish = simulate_fish(fish);
@@ -54,11 +102,25 @@ void day06()
         std::cout << std::endl;
         */
     }
+    unsigned int part_1_solution = fish.size();
 
+    // Part 2
+    num_days = 256;
+    fish = initial_state;
 
+    Fish_simulator simulator(fish);
+
+    for (int i=0; i<num_days; i++) {
+        simulator.simulate();
+        //std::cout << "After " << i+1 << " days: ";
+        //simulator.print_buckets();
+    }
+    //std::cout << "After " << num_days << " days: ";
+    //std::cout << simulator.get_num_fish() << std::endl;
+    uint64_t part_2_solution = simulator.get_num_fish();
 
     // Display final results
     std::cout << "Day 6:" << std::endl;
-    std::cout << fish.size() << std::endl;
-    std::cout << -1 << std::endl;
+    std::cout << part_1_solution << std::endl;
+    std::cout << part_2_solution << std::endl;
 }
