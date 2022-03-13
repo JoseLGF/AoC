f23cafb988d83c21d4581b0e06ac6cfb0382f197
Jose Luis <luisgermanfelix@gmail.com>
2022-03-14 00:49:26 +0100

    Add second part of day 10

--- 2021/src/day10.cpp
+++ 2021/src/day10.cpp
@@ -56,10 +56,78 @@ int calculate_syntax_error_score(std::string line)
     return 0;
 }
 
+std::string get_completing_characters(std::string line)
+{
+    // Returns the characters necessary to close the current
+    // line. If the line is complete, or if it is corrupted,
+    // return an empty string
+    std::stack<char> chars;
+
+    for (auto c: line) {
+        // Opening parens are always accepted
+        if (c == '(' ||
+            c == '[' ||
+            c == '{' ||
+            c == '<')
+        {
+            chars.push(c);
+        }
+        else {
+            // A closing paren on an empty stack is error
+            if (chars.empty()) {
+                return "";
+            }
+            // closing parens must match top of stack
+            if (
+                 ((c == ')') && (chars.top() == '(')) ||
+                 ((c == ']') && (chars.top() == '[')) ||
+                 ((c == '}') && (chars.top() == '{')) ||
+                 ((c == '>') && (chars.top() == '<'))
+               )
+            {
+                chars.pop();
+            }
+            else {
+                return "";
+            }
+        }
+    }
+
+    std::string out;
+    while(! chars.empty()) {
+        switch (chars.top()) {
+            case '(' : out += ')'; break;
+            case '[' : out += ']'; break;
+            case '{' : out += '}'; break;
+            case '<' : out += '>'; break;
+        }
+        chars.pop();
+    }
+
+    return out;
+}
+
+unsigned long get_autocomplete_score(std::string completing_chars)
+{
+    unsigned long score = 0;
+
+    for (auto c: completing_chars) {
+        score *= 5;
+        switch(c) {
+            case ')': score += 1; break;
+            case ']': score += 2; break;
+            case '}': score += 3; break;
+            case '>': score += 4; break;
+        }
+    }
+
+    return score;
+}
+
 void day10()
 {
     int part_1_solution = -1;
-    int part_2_solution = -1;
+    unsigned long part_2_solution = -1;
     std::ifstream infile("input/day10.txt");
 
     // Parse data
@@ -75,6 +143,21 @@ void day10()
         syntax_error_score += calculate_syntax_error_score(l);
     }
     part_1_solution = syntax_error_score;
+
+    // Part 2
+    std::vector<unsigned long> autocomplete_scores;
+    for (auto l: lines) {
+        unsigned long current_score =
+            get_autocomplete_score(
+                    get_completing_characters(l));
+        if (0 != current_score) {
+            autocomplete_scores.push_back(current_score);
+        }
+    }
+    std::sort(autocomplete_scores.begin(),
+              autocomplete_scores.end());
+    part_2_solution = autocomplete_scores[
+        autocomplete_scores.size()/2];
 
 
     // Display final results
