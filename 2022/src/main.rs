c427e34450b5de7e7223b7be79d1ca3271179d15
Jose Luis <luisgermanfelix@gmail.com>
2022-12-04 16:44:32 +0100

    Add solution for 2022D01P2

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -13,7 +13,7 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
         .collect()
 }
 
-fn day_01_1(lines: Vec<String>) -> u32 {
+fn day_01_1(lines: &Vec<String>) -> u32 {
     let mut max = 0;
     let mut current = 0;
     for line in lines {
@@ -35,6 +35,32 @@ fn day_01_1(lines: Vec<String>) -> u32 {
     return max;
 }
 
+fn day_01_2(lines: &Vec<String>) -> u32 {
+    let mut calories_counts = Vec::new();
+    let mut current = 0;
+
+    for line in lines {
+        if line.is_empty() {
+            calories_counts.push(current);
+            current = 0;
+        }
+        else {
+            let input: u32 = line
+            .trim()
+            .parse()
+            .expect("Wanted a number");
+
+            current += input;
+        }
+    }
+    
+    calories_counts.sort_by(|a, b| b.cmp(a));
+
+    return  calories_counts[0] +
+            calories_counts[1] +
+            calories_counts[2];
+}
+
 fn main() {
     println!("Advent of code 2022 solutions:");
 
@@ -43,7 +69,8 @@ fn main() {
     let lines = lines_from_file("./input/day_01.txt");
 
     println!("Day 01:");
-    println!("{}", day_01_1(lines));
+    println!("{}", day_01_1(&lines));
+    println!("{}", day_01_2(&lines));
 
     
 }
