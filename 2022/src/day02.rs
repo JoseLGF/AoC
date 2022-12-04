a5ff9a4ce732f661850575434297906b05925e0b
Jose Luis <luisgermanfelix@gmail.com>
2022-12-04 20:07:34 +0100

    Add solution to 2022D2P2

--- 2022/src/day02.rs
+++ 2022/src/day02.rs
@@ -14,5 +14,15 @@ pub fn part_1(lines: &Vec<String>) -> u32 {
 }
 
 pub fn part_2(lines: &Vec<String>) -> u32 {
-    return 0;
+    let lookup = [[3, 4, 8], [1, 5, 9], [2, 6, 7]];
+    let mut total_score = 0;
+
+    for line in lines {
+        let his = line.as_bytes()[0] - b'A';
+        let mine = line.as_bytes()[2] - b'X';
+
+        total_score += lookup[his as usize][mine as usize];
+    }
+
+    return total_score;
 }
