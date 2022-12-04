cd96a4ef1b78c0db386e26a7f94c0c3461bdf90f
Jose Luis <luisgermanfelix@gmail.com>
2022-12-04 18:06:35 +0100

    Add solution for 2022D2P1

---
+++ 2022/src/day02.rs
@@ -0,0 +1,18 @@
+pub fn part_1(lines: &Vec<String>) -> u32 {
+
+    let lookup = [[4, 8, 3], [1, 5, 9], [7, 2, 6]];
+    let mut total_score = 0;
+
+    for line in lines {
+        let his = line.as_bytes()[0] - b'A';
+        let mine = line.as_bytes()[2] - b'X';
+
+        total_score += lookup[his as usize][mine as usize];
+    }
+
+    return total_score;
+}
+
+pub fn part_2(lines: &Vec<String>) -> u32 {
+    return 0;
+}
--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -1,5 +1,6 @@
 mod helpers;
 mod day01;
+mod day02;
 
 fn main() {
     println!("Advent of code 2022 solutions:");
@@ -9,4 +10,9 @@ fn main() {
     println!("Day 01:");
     println!("{}", day01::part_1(&lines));
     println!("{}", day01::part_2(&lines));
+
+    let lines2 = helpers::lines_from_file("./input/day_02.txt");
+    println!("Day 02:");
+    println!("{}", day02::part_1(&lines2));
+    println!("{}", day02::part_2(&lines2));
 }
