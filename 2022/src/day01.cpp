9cd8bb524da93433ca868c87f9e4535d4af90469
Jose Luis <luisgermanfelix@gmail.com>
2023-05-06 23:28:47 +0200

    add 2021 day 01 part 2 c++ solution

--- 2022/src/day01.cpp
+++ 2022/src/day01.cpp
@@ -3,16 +3,9 @@
 #include <fstream>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
-int day01_1(std::string& input_path) {
-    std::ifstream infile(input_path);
-    std::vector<std::string> lines;
-    std::string line;
-    while (std::getline(infile, line))
-    {
-        lines.push_back(line);
-    }
-
+int day01_1(std::vector<std::string>& lines) {
     int max = 0;
     int current = 0;
     for (auto line: lines) {
@@ -30,7 +23,41 @@ int day01_1(std::string& input_path) {
     return max;
 }
 
+int day01_2(std::vector<std::string>& lines) {
+    std::vector<int> calories_counts;
+    int current = 0;
+
+    for (auto line: lines) {
+        if (line.empty()) {
+            calories_counts.push_back(current);
+            current = 0;
+        }
+        else {
+            int calories = std::stoi(line);
+            current += calories;
+        }
+    }
+
+    std::sort(calories_counts.begin(),
+              calories_counts.end(),
+              std::greater<int>());
+
+    return  calories_counts[0] +
+            calories_counts[1] +
+            calories_counts[2];
+}
+
 void day01(std::string& input_path) {
     std::cout << "Day 1:" << std::endl;
-    std::cout << day01_1(input_path) << std::endl;
+
+    std::ifstream infile(input_path);
+    std::vector<std::string> lines;
+    std::string line;
+    while (std::getline(infile, line))
+    {
+        lines.push_back(line);
+    }
+
+    std::cout << day01_1(lines) << std::endl;
+    std::cout << day01_2(lines) << std::endl;
 }
